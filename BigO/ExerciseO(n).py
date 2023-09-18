8e8f31fd4a05359b35e73ad47553401d0b232c61
Douglas Amorim <amorimdos@gmail.com>
2023-09-18 14:11:58 -0300

    feat simple O(n) exercise

---
+++ BigO/ExerciseO(n).py
@@ -0,0 +1,11 @@
+# Implement a Python function called print_items. 
+# This function should take a single integer as an argument and print out a sequence of numbers from 0 up to, but not including, the provided integer.
+# The function should use a for loop and Python's built-in range function to generate the sequence of numbers.
+
+# The function signature should be: def print_items(n):
+
+def print_items(n): 
+    for i in range(n - 1): 
+        print(i);
+        
+print_items(10);
---
+++ BigO/ReviewingBigO.py
@@ -0,0 +1,8 @@
+# Example of O(n) function
+# because N is the number of operations made
+# O(n) is also call 'proportional'
+def print_items(n): 
+    for i in range(n): 
+        print(i)
+
+print_items(10)
