83fcbb700f600d8f80089aae0434e2b99bb54f71
Douglas Amorim <amorimdos@gmail.com>
2023-10-05 15:22:25 -0300

    feat Doubly linked list popFirst method

--- DoubleLinkedList/DoubleLinkedList.py
+++ DoubleLinkedList/DoubleLinkedList.py
@@ -63,6 +63,22 @@ class DoublyLinkedList:
         
         self.length += 1
         return True
+    
+    def pop_first(self): 
+        if self.length == 0: 
+            return None
+        
+        temp = self.head
+        if self.length == 1: 
+            self.head = None
+            self.tail = None
+        else: 
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+
+        self.length -= 1
+        return temp
         
 myDoublyLinkedList = DoublyLinkedList(7)
 myDoublyLinkedList.append(2)
@@ -70,4 +86,5 @@ myDoublyLinkedList.append(5)
 myDoublyLinkedList.append(9)
 myDoublyLinkedList.prepend(15)
 myDoublyLinkedList.pop()
+myDoublyLinkedList.pop_first()
 myDoublyLinkedList.print_list()
