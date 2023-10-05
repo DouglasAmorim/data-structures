e3470fcc2a30bb4994a079e712ca9178e6fffd6b
Douglas Amorim <amorimdos@gmail.com>
2023-10-05 14:50:53 -0300

    feat doubly linked list append method

--- DoubleLinkedList/DoubleLinkedList.py
+++ DoubleLinkedList/DoubleLinkedList.py
@@ -17,6 +17,22 @@ class DoublyLinkedList:
         while temp is not None: 
             print(temp.value)
             temp = temp.next
+    
+    def append(self, value): 
+        newNode = Node(value)
+
+        if self.head == None: 
+            self.head = newNode
+            self.tail = newNode
+        else: 
+            newNode.prev = self.tail
+            self.tail.next = newNode
+            self.tail = newNode
+
+        self.length += 1
+        return True
+        
 
 myDoublyLinkedList = DoublyLinkedList(7)
+myDoublyLinkedList.append(2)
 myDoublyLinkedList.print_list()
