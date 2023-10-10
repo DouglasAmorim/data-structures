9a0d4a61bef2ea8ec0541b07f27facc21aed6a7b
Douglas Amorim <amorimdos@gmail.com>
2023-10-10 15:09:09 -0300

    feat stack constructor

---
+++ Stack/Stack.py
@@ -0,0 +1,20 @@
+class Node: 
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self, value):
+        newNode = Node(value)
+        self.top = newNode
+        self.height = 1
+
+    def print_stack(self): 
+        temp = self.top
+
+        while temp is not None: 
+            print(temp.value)
+            temp = temp.next
+
+myStack = Stack(5)
+myStack.print_stack()
