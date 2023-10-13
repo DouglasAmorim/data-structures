1f03086b82575d91b748ba27b01a7a6a206c8e78
Douglas Amorim <amorimdos@gmail.com>
2023-10-13 17:16:44 -0300

    feat stack reverse string method

--- Stack/Stack-InterviewQuestions/StackUsingList.py
+++ Stack/Stack-InterviewQuestions/StackUsingList.py
@@ -27,7 +27,30 @@ class Stack:
         
         self.stack_list.pop()
 
+def is_balanced_parentheses(parentheses):
+    stack = Stack()
+    for p in parentheses:
+        if p == '(':
+            stack.push(p)
+        elif p == ')':
+            if stack.is_empty() or stack.pop() != '(':
+                return False
+    return stack.is_empty()
+
+def reverse_string(string): 
+    stack = Stack()
+    reversed_string = ""
+ 
+    for char in string:
+        stack.push(char)
+ 
+    while not stack.is_empty():
+        reversed_string += stack.pop()
+ 
+    return reversed_string
+
 myStack = Stack()
 myStack.push(3)
+print(reverse_string("banana"))
 #myStack.pop()
 myStack.print_stack()
