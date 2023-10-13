95cd47dcbbebe635fa676f6f46b84716055d7458
Douglas Amorim <amorimdos@gmail.com>
2023-10-13 17:26:47 -0300

    feat queue dequeue method using stack

--- Queues/Queues-InterviewQuestions/QueueUsingStack.py
+++ Queues/Queues-InterviewQuestions/QueueUsingStack.py
@@ -10,7 +10,11 @@ class MyQueue:
         while len(self.stack2) > 0:
             self.stack1.append(self.stack2.pop())
 
-        self.stack1.append(value)
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack1.pop()
 
 myQueue = MyQueue()
 myQueue.enqueue(7)
