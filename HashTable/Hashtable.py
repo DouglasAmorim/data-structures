606c63e33df448164773d5f3702a3a6aedcbc0a1
Douglas Amorim <amorimdos@gmail.com>
2023-10-20 13:21:26 -0300

    feat hash table get method

--- HashTable/Hashtable.py
+++ HashTable/Hashtable.py
@@ -19,10 +19,26 @@ class HashTable:
             self.data_map[index] = []
         self.data_map[index].append([key, value])
 
+    def get(self, key): 
+        index = self.__hash(key)
+
+        if self.data_map[index] is not None: 
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key: 
+                    return self.data_map[index][i][1]
+        
+        return None
+    
+        
+
+
 
 myHashTable = HashTable()
 myHashTable.set("emergency", 911)
 myHashTable.set("call", 912)
 myHashTable.set("the", 12)
 myHashTable.set("police", 999)
+
+print(myHashTable.get("teste"))
+print(myHashTable.get("police"))
 myHashTable.print_table()
