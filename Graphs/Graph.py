5a9ce57fc1af41446c9ad32b57d9d6be8538eb5a
Douglas Amorim <amorimdos@gmail.com>
2023-11-06 15:54:44 -0300

    feat created init and add method for graph

--- Graphs/Graph.py
+++ Graphs/Graph.py
@@ -7,4 +7,12 @@ class Graph:
             self.adj_list[vertex] = []
             return True
         
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ":", self.adj_list[vertex])
+        
         return False
+    
+myGraph = Graph()
+myGraph.add_vertex('5')
+myGraph.print_graph()
