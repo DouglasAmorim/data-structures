7e1af55cfd48177e7ab5f2db2ae94db61d65b2fe
Douglas Amorim <amorimdos@gmail.com>
2023-11-08 16:48:38 -0300

    feat created remove vertex method in Graphs

--- Graphs/Graph.py
+++ Graphs/Graph.py
@@ -12,17 +12,36 @@ class Graph:
         if vertex not in self.adj_list.keys():
             self.adj_list[vertex] = []
             return True
-        
+        return False
+    
     def add_edge(self, v1, v2): 
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
             self.adj_list[v1].append(v2)
             self.adj_list[v2].append(v1)
             return True
         return False
-
+    
+    def remove_edge(self, v1, v2): 
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except:
+                pass
+            return True
+        return False
     
 myGraph = Graph()
-myGraph.add_vertex(5)
+myGraph.add_vertex(1)
+myGraph.add_vertex(2)
 myGraph.add_vertex(3)
-myGraph.add_edge(5, 3)
+myGraph.add_vertex(4)
+
+myGraph.add_edge(1, 2)
+myGraph.add_edge(2, 3)
+myGraph.add_edge(3, 1)
+
+myGraph.remove_edge(1, 2)
+myGraph.remove_edge(1, 4)
+
 myGraph.print_graph()
