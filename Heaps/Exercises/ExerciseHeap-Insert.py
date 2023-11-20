efcd127686b67913ad9d7752bc9c7be949dca0e6
Douglas Amorim <amorimdos@gmail.com>
2023-11-20 13:37:11 -0300

    feat created Heap insert method

--- Heaps/Exercises/ExerciseHeap-Insert.py
+++ Heaps/Exercises/ExerciseHeap-Insert.py
@@ -2,16 +2,16 @@ class MaxHeap:
     def __init__(self):
         self.heap = []
 
-    def _left_child(self, index): 
+    def _left_child(self, index):
         return 2 * index + 1
-    
-    def _right_child(self, index): 
+
+    def _right_child(self, index):
         return 2 * index + 2
-    
-    def _parent(self, index): 
+
+    def _parent(self, index):
         return (index - 1) // 2
-    
-    def _swap(self, index1, index2): 
+
+    def _swap(self, index1, index2):
         self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
 
     def insert(self, value):
@@ -23,12 +23,32 @@ class MaxHeap:
             current = self._parent(current)
 
 
-myHeap = MaxHeap()
-myHeap.insert(99)
-myHeap.insert(72)
-myHeap.insert(61)
-myHeap.insert(58)
-myHeap.insert(100)
-myHeap.insert(75)
 
-print(myHeap.heap)
+myheap = MaxHeap()
+myheap.insert(99)
+myheap.insert(72)
+myheap.insert(61)
+myheap.insert(58)
+
+print(myheap.heap)  
+
+
+myheap.insert(100)
+
+print(myheap.heap)  
+
+
+myheap.insert(75)
+
+print(myheap.heap)
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [99, 72, 61, 58]
+    [100, 99, 61, 58, 72]
+    [100, 99, 75, 58, 72, 61]
+
+"""
+
