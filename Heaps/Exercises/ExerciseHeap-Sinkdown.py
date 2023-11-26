c03081420495808edd8cd8138fce114a1c5de546
Douglas Amorim <amorimdos@gmail.com>
2023-11-26 14:04:44 -0300

    feat implemented SinkDown method to a Heap

--- Heaps/Exercises/ExerciseHeap-Sinkdown.py
+++ Heaps/Exercises/ExerciseHeap-Sinkdown.py
@@ -43,15 +43,14 @@ class MaxHeap:
             else:
                 return
                        
-    def remove(self): 
-        if len(self.heap) == 0: 
+    def remove(self):
+        if len(self.heap) == 0:
             return None
-        
-        if len(self.heap) == 1: 
+
+        if len(self.heap) == 1:
             return self.heap.pop()
 
         max_value = self.heap[0]
-
         self.heap[0] = self.heap.pop()
         self._sink_down(0)
 
